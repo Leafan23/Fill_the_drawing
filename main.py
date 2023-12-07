abe401664b9e68f1822cd5a36a098f9754244799
Leafan23 <la2player_07@mail.ru>
2023-12-07 15:23:41 +0300

    v0.1 (Build 1) Fill the drawing
    Создание проекта

---
+++ main.py
@@ -0,0 +1,38 @@
+import pythoncom
+from win32com.client import Dispatch, gencache, VARIANT
+
+
+class KompasAPI:
+    def __init__(self):
+        #  Подключим описание интерфейсов API7
+        self.api7 = gencache.EnsureModule("{69AC2981-37C0-4379-84FD-5DD2F3C0A520}", 0, 1, 0)
+        self.application = self.api7.IApplication(
+            Dispatch("Kompas.Application.7")._oleobj_.QueryInterface(self.api7.IApplication.CLSID, pythoncom.IID_IDispatch))
+
+        self.kompas_document = self.application.ActiveDocument
+        self.kompas_document_3d = self.api7.IKompasDocument3D(self.kompas_document)
+        self.part_7 = self.kompas_document_3d.TopPart
+        self.property_keeper = self.api7.IPropertyKeeper(self.part_7)
+        self.property_mng = self.api7.IPropertyMng(self.application)
+
+
+if __name__ == "__main__":
+    invent_surname = 120  # номер графы Фамилии разработчика
+    company_name = 9 # номер графы фирмы
+
+
+    # Подключение к API компаса
+    kompas_api = KompasAPI()
+    lay_out_sheets = kompas_api.kompas_document.LayoutSheets
+    lay_out_sheet = lay_out_sheets.ItemByNumber(1)
+
+    stamp = lay_out_sheet.Stamp
+    text = stamp.Text(company_name)
+
+    text.Str = 'ООО "Горные технологии \nи инновации"'
+
+    stamp.Update()
+
+    #
+
+
